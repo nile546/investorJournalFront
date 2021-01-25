ec059787d1e961d5f262a44eacc37440576ec0a9
Nikita <nikez@inbox.ru>
2021-01-25 23:34:17 +0300

    create landing menu

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from "@angular/core";
 import { LandingRoutingModule } from "./Landing-routing.module";
 import { LandingComponent } from "./landing.component";
 import { HeaderComponent } from './header/header.component';
+import { ComponentsModule } from "src/app/shared/components/components.module";
+import { MainMenuComponent } from "./shared/components/main-menu/main-menu.component";
 
 
 @NgModule({
     declarations:[
         LandingComponent,
-        HeaderComponent
+        HeaderComponent,
+        MainMenuComponent
     ],
     imports:[
         LandingRoutingModule,
+        ComponentsModule,
     ],
 })
 export class LandingModule{
---
+++ src/app/modules/landing/shared/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+
+
+@NgModule({
+  declarations: [MainMenuComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MainMenuComponent
+  ]
+})
+export class ComponentsModule { }
