bc8ddc9305f415cb793a36f425c219c14ca9a41b
Denis Anisimov <vysheradugi4@gmail.com>
2021-04-21 16:39:57 -0400

    Slider pictures added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  sliderDuration: 5000,
+  sliderDuration: 10000,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  sliderDuration: 5000,
+  sliderDuration: 10000,
 };
 
 /*
