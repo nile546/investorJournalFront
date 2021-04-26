d40f8aebec0f291d72befcf557da1521aaec8e79
Denis Anisimov <vysheradugi4@gmail.com>
2021-04-26 05:56:52 -0400

    Overflow hidden added for body when modal opened

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,7 +1,9 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { ModalActions } from 'src/app/store/modal/modal.actions';
 import { ModalSelectors } from 'src/app/store/modal/modal.selectors';
 
@@ -16,13 +18,32 @@ export class ModalComponent implements OnInit {
 
   public isModalOpen: Observable<boolean> | undefined;
 
+  private _unsubscribe: Subject<boolean> = new Subject<boolean>();
+
+
   constructor(
     private _store: Store,
+    private _renderer: Renderer2,
+    @Inject(DOCUMENT) private _document: Document,
   ) { }
 
 
   ngOnInit(): void {
     this.isModalOpen = this._store.select(ModalSelectors.isModalOpen);
+
+
+    // Overflow is hidden for body when modal opened.
+    this.isModalOpen.pipe(
+      takeUntil(this._unsubscribe),
+    )
+      .subscribe((status: boolean) => {
+        if (status) {
+          this._renderer.setStyle(this._document.body, 'overflow', 'hidden');
+          return;
+        }
+
+        this._renderer.setStyle(this._document.body, 'overflow', 'auto');
+      });
   }
 
 
