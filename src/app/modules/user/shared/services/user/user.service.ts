133aaa35824961c7b671241d418310efeb2e2f48
Nikita <nikez@inbox.ru>
2021-05-08 01:04:25 +0300

    mod user service

--- src/app/modules/user/shared/services/user/user.service.ts
+++ src/app/modules/user/shared/services/user/user.service.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs';
 
 import { Result } from 'src/app/shared/models/user/result/result.model';
 import { User } from 'src/app/shared/models/user/user.model';
-import { environment } from 'src/environments/environment';
 
 
 @Injectable()
@@ -17,8 +16,12 @@ export class UserService {
 
   public signup(user: User): Observable<Result> {
     return this._httpClient.post<Result>(
-      'signup',
-      { user }
+      'users/signup',
+      {
+        login: user.login,
+        email: user.email,
+        password: user.password,
+      }
     )
   }
 }
