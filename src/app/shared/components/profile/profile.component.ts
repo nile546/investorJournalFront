567bb43bff32f3dd7756fc37a537e61e71f157fe
Denis Anisimov <vysheradugi4@gmail.com>
2021-05-26 16:38:21 -0400

    Icon profile fixed

--- src/app/shared/components/profile/profile.component.ts
+++ src/app/shared/components/profile/profile.component.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+import { UserSelectors } from 'src/app/modules/user/store/user/user.selectors';
 import { ModalActions } from 'src/app/store/modal/modal.actions';
 import { State } from 'src/app/store/root.reducer';
+import { User } from '../../models/user/user.model';
 import { ModalComponents } from '../modal/modal.component';
 
 
@@ -12,11 +15,18 @@ import { ModalComponents } from '../modal/modal.component';
   styleUrls: ['./profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  public currentUser: Observable<User | null> | undefined;
 
   constructor(
     private _store: Store<State>
   ) { }
+
+
+  ngOnInit(): void {
+    this.currentUser = this._store.select(UserSelectors.currentUser);
+  }
 
 
   public signin(): void {
